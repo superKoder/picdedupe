c9f1a33b8e88bfe6e0b184823ef007c335bb3231
superKoder <tim.koder@gmail.com>
2022-12-24 01:25:44 -0800

    Introducing picdeduper.fileseries
    A PictureFileSeries is a gap-less sequence of pictures taken at the same time, at the same place, by the same creator.

---
+++ picdeduper/fileseries.py
@@ -0,0 +1,137 @@
+from picdeduper import platform as pds
+from picdeduper import common as pdc
+from picdeduper import filegroups
+from picdeduper import latlngs as pdl
+from picdeduper import time as pdt
+from picdeduper import images
+
+from abc import ABC, abstractmethod
+from typing import List
+
+
+class PictureFileSeries(ABC):
+    """
+    A PictureFileSeries is a gap-less sequence of pictures taken at the 
+    same time, at the same place, by the same creator.
+    
+    In reality, it stores PictureFileGroups, rather than individual files.
+    """
+
+    def __init__(self,
+                 file_num: int,
+                 latlng: pdl.LatLng,
+                 timestamp: pdt.Timestamp,
+                 creator: str) -> None:
+
+        super().__init__()
+        self.file_num = file_num
+        self.latlng = latlng
+        self.timestamp = timestamp
+        self.creator = creator
+        self.file_groups: List[filegroups.PictureFileGroup] = list()
+
+    def add_file_group(self, file_group: filegroups.PictureFileGroup) -> None:
+        self.file_groups.append(file_group)
+
+
+class PictureFileSeriesSplitter:
+    """
+    Mechanism to split a stream of files (PictureFileGroups) into PictureFileSeries.
+    """
+
+    def __init__(self,
+                 max_file_num_gap=0,
+                 max_distance_degrees=10,
+                 max_timestamp_diff: pdt.Timestamp = 8*3600) -> None:
+
+        self.all_file_series: List[PictureFileSeries] = list()
+        self.curr_file_series: PictureFileSeries = None
+        self.curr_file_group: filegroups.PictureFileGroup = None
+        self.curr_file_num: int = None
+
+        self.max_file_num_gap = max_file_num_gap
+        self.max_distance_degrees = max_distance_degrees
+        self.max_timestamp_diff = max_timestamp_diff
+
+    def _is_file_num_gap(self, file_num: int) -> bool:
+        if not self.curr_file_series:
+            return True
+        if (file_num <= self.curr_file_num):
+            return True
+        return (file_num > self.curr_file_num + self.max_file_num_gap + 1)
+
+    def _is_too_far_away(self, latlng: pdl.LatLng) -> bool:
+        if not self.curr_file_series:
+            return True
+        if self.curr_file_series.latlng == latlng:
+            return False
+        if not self.curr_file_series.latlng:
+            return True
+        return ((self.curr_file_series.latlng.distance_in_km(latlng)) > self.max_distance_degrees)
+
+    def _is_too_new(self, timestamp: pdt.Timestamp) -> bool:
+        if not self.curr_file_series:
+            return True
+        if self.curr_file_series.timestamp == timestamp:
+            return False
+        if not self.curr_file_series.timestamp:
+            return True
+        return (pdt.seconds_between_times(timestamp, self.curr_file_series.timestamp) > self.max_timestamp_diff)
+
+    def _is_other_creator(self, creator: str) -> bool:
+        if not self.curr_file_series:
+            return True
+        return (self.curr_file_series.creator != creator)
+
+    def _start_new_series(self,
+                          file_num: int,
+                          latlng: pdl.LatLng,
+                          timestamp: pdt.Timestamp,
+                          creator: str) -> None:
+        self.curr_file_series = PictureFileSeries(file_num, latlng, timestamp, creator)
+        self.all_file_series.append(self.curr_file_series)
+
+    def _needs_new_series(self,
+                          file_num: int,
+                          latlng: pdl.LatLng,
+                          timestamp: pdt.Timestamp,
+                          creator: str) -> bool:
+
+        if self._is_file_num_gap(file_num):
+            return True
+
+        if self._is_too_far_away(latlng):
+            return True
+
+        if self._is_too_new(timestamp):
+            return True
+
+        if self._is_other_creator(creator):
+            return True
+
+        return False
+
+    def add_path(self,
+                 path: pds.Path,
+                 properties: pdc.PropertyDict) -> None:
+
+        file_prefix, file_num = images.filename_dcf_prefix_and_number(path)
+        creator = properties[pdc.KEY_IMAGE_CREATOR]
+        latlng: pdl.LatLng = pdl.parse_latlng(properties[pdc.KEY_IMAGE_LOC])
+        timestamp: pdt.Timestamp = pdt.timestamp_from_string(properties[pdc.KEY_IMAGE_DATE])
+
+        if self._needs_new_series(file_num, latlng, timestamp, creator):
+            self._start_new_series(file_num, latlng, timestamp, creator)
+
+        assert self.curr_file_series
+
+        if ((self.curr_file_group) and
+            (file_num == self.curr_file_num) and
+                (self.curr_file_group.could_include_path(path))):
+            self.curr_file_group.add_file_path(path)
+            return
+
+        self.curr_file_group = filegroups.PictureFileGroup()
+        self.curr_file_group.add_file_path(path)
+        self.curr_file_series.add_file_group(self.curr_file_group)
+        self.curr_file_num = file_num
---
+++ tests/test_fileseries.py
@@ -0,0 +1,139 @@
+import unittest
+
+from picdeduper import common as pdc
+from picdeduper import fileseries
+from picdeduper import latlngs
+
+
+class FileSeriesTests(unittest.TestCase):
+
+    def test_perfect_series(self):
+
+        splitter = fileseries.PictureFileSeriesSplitter()
+
+        self.assertEqual(len(splitter.all_file_series), 0)
+
+        properties = {
+            pdc.KEY_IMAGE_CREATOR: "creator",
+            pdc.KEY_IMAGE_LOC: "<+37.4,-120.3>",
+            pdc.KEY_IMAGE_DATE: "2022-12-23 20:13:32 -0700"
+        }
+
+        splitter.add_path("/path/one/IMG_1001.JPG", properties)
+        splitter.add_path("/path/one/IMG_1002.JPG", properties)
+        splitter.add_path("/path/one/IMG_1003.JPG", properties)
+        splitter.add_path("/path/one/IMG_1004.JPG", properties)
+        self.assertEqual(len(splitter.all_file_series), 1)
+
+    def test_with_gap(self):
+
+        splitter = fileseries.PictureFileSeriesSplitter()
+
+        self.assertEqual(len(splitter.all_file_series), 0)
+
+        properties = {
+            pdc.KEY_IMAGE_CREATOR: "creator",
+            pdc.KEY_IMAGE_LOC: "<+37.4,-120.3>",
+            pdc.KEY_IMAGE_DATE: "2022-12-23 20:13:32 -0700"
+        }
+
+        splitter.add_path("/path/one/IMG_1001.JPG", properties)
+        splitter.add_path("/path/one/IMG_1002.JPG", properties)
+
+        splitter.add_path("/path/one/IMG_1004.JPG", properties)
+        splitter.add_path("/path/one/IMG_1005.JPG", properties)
+
+        self.assertEqual(len(splitter.all_file_series), 2)
+
+    def test_different_location(self):
+
+        splitter = fileseries.PictureFileSeriesSplitter()
+
+        self.assertEqual(len(splitter.all_file_series), 0)
+
+        location_a = {
+            pdc.KEY_IMAGE_CREATOR: "creator",
+            pdc.KEY_IMAGE_LOC: latlngs.PARIS.as_string(),
+            pdc.KEY_IMAGE_DATE: "2022-12-23 20:13:32 -0700"
+        }
+
+        location_b = {
+            pdc.KEY_IMAGE_CREATOR: "creator",
+            pdc.KEY_IMAGE_LOC: latlngs.BRUSSELS.as_string(),  # !!!
+            pdc.KEY_IMAGE_DATE: "2022-12-23 20:13:32 -0700"
+        }
+
+        splitter.add_path("/path/one/IMG_1001.JPG", location_a)
+        splitter.add_path("/path/one/IMG_1002.JPG", location_a)
+        splitter.add_path("/path/one/IMG_1003.JPG", location_a)
+
+        splitter.add_path("/path/one/IMG_1004.JPG", location_b)
+        splitter.add_path("/path/one/IMG_1005.JPG", location_b)
+
+        self.assertEqual(len(splitter.all_file_series), 2)
+        file_series_at_paris = splitter.all_file_series[0]
+        file_series_at_brussels = splitter.all_file_series[1]
+        self.assertEqual(len(file_series_at_paris.file_groups), 3)
+        self.assertEqual(len(file_series_at_brussels.file_groups), 2)
+
+    def test_different_time(self):
+
+        splitter = fileseries.PictureFileSeriesSplitter()
+
+        self.assertEqual(len(splitter.all_file_series), 0)
+
+        time_a = {
+            pdc.KEY_IMAGE_CREATOR: "creator",
+            pdc.KEY_IMAGE_LOC: latlngs.NEW_YORK.as_string(),
+            pdc.KEY_IMAGE_DATE: "2022-12-23 20:13:32 -0700"
+        }
+
+        time_b = {
+            pdc.KEY_IMAGE_CREATOR: "creator",
+            pdc.KEY_IMAGE_LOC: latlngs.NEW_YORK.as_string(),
+            pdc.KEY_IMAGE_DATE: "2022-12-24 20:13:32 -0700"  # !!!
+        }
+
+        splitter.add_path("/path/one/IMG_1001.JPG", time_a)
+        splitter.add_path("/path/one/IMG_1002.JPG", time_a)
+
+        splitter.add_path("/path/one/IMG_1003.JPG", time_b)
+        splitter.add_path("/path/one/IMG_1004.JPG", time_b)
+        splitter.add_path("/path/one/IMG_1005.JPG", time_b)
+
+        self.assertEqual(len(splitter.all_file_series), 2)
+        file_series_on_23rd = splitter.all_file_series[0]
+        file_series_on_24th = splitter.all_file_series[1]
+        self.assertEqual(len(file_series_on_23rd.file_groups), 2)
+        self.assertEqual(len(file_series_on_24th.file_groups), 3)
+
+    def test_different_creator(self):
+
+        splitter = fileseries.PictureFileSeriesSplitter()
+
+        self.assertEqual(len(splitter.all_file_series), 0)
+
+        creator_a = {
+            pdc.KEY_IMAGE_CREATOR: "creator",
+            pdc.KEY_IMAGE_LOC: latlngs.NEW_YORK.as_string(),
+            pdc.KEY_IMAGE_DATE: "2022-12-23 20:13:32 -0700"
+        }
+
+        creator_b = {
+            pdc.KEY_IMAGE_CREATOR: "other",  # !!!
+            pdc.KEY_IMAGE_LOC: latlngs.NEW_YORK.as_string(),
+            pdc.KEY_IMAGE_DATE: "2022-12-23 20:13:32 -0700"
+        }
+
+        splitter.add_path("/path/one/IMG_1001.JPG", creator_a)
+        splitter.add_path("/path/one/IMG_1002.JPG", creator_a)
+        splitter.add_path("/path/one/IMG_1003.JPG", creator_a)
+        splitter.add_path("/path/one/IMG_1004.JPG", creator_a)
+
+        splitter.add_path("/path/one/IMG_1005.JPG", creator_b)
+
+        self.assertEqual(len(splitter.all_file_series), 2)
+        file_series_of_creator1 = splitter.all_file_series[0]
+        file_series_of_creator2 = splitter.all_file_series[1]
+        self.assertEqual(len(file_series_of_creator1.file_groups), 4)
+        self.assertEqual(len(file_series_of_creator2.file_groups), 1)
